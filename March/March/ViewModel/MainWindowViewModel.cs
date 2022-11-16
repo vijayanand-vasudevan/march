b442c02a55c67273a4673438df7e06a76f092b75
Vijayanand Vasudevan <vijayanand.vasudevan@gmail.com>
2022-11-16 07:48:31 +0000

    My first UI

---
+++ March/March/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using VUtils;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace March.ViewModel
+{
+    class MainWindowViewModel
+    {
+        private ICommand m_ButtonCommand;
+        public ICommand ButtonCommand
+        {
+            get
+            {
+                return m_ButtonCommand;
+            }
+            set
+            {
+                m_ButtonCommand = value;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            ButtonCommand = new RelayCommand(new Action<object>(ShowMessage));
+        }
+
+        public void ShowMessage(object obj)
+        {
+            MessageBox.Show(obj.ToString());
+
+            string assembly = "";
+            string xaml = "March.Views.UserList";
+            string view = "March.ViewModel.UserList";
+
+            Assembly ta = null;
+            if (string.IsNullOrEmpty(assembly))
+                ta = Assembly.GetCallingAssembly();
+            else
+                ta = Assembly.Load(assembly);
+
+            Type wndtp = ta.GetType(xaml);
+            Window wnd = new Window();
+            UserControl xs = (UserControl)Activator.CreateInstance(wndtp);
+            wnd.Content = xs;
+            //wnd.DataContext = xs;
+            Type vwtp = ta.GetType(view);
+            var vwm = Activator.CreateInstance(vwtp);
+            xs.DataContext = vwm;
+            xs.Height = wnd.Height;
+            xs.Width = wnd.Width;
+            wnd.Show();
+        }
+    }
+}
---
+++ March/March/ViewModel/UserList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace March.ViewModel
+{
+    class UserList : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public string Comments { get; set; }
+            public int Age { get; set; }
+        };
+
+        private List<Person> Persons = new List<Person>();
+        public IEnumerable<Person> MyCollection
+        {
+            get {  return (from v in Persons select v); }
+        }
+        private Timer _myTimer;
+
+        void genData()
+        {
+            int iL = Persons.Count;
+            for (int i = iL; i < iL+200; i++)
+            {
+                Person p = new Person();
+                p.Name = "Vijay" + i.ToString();
+                p.Age = i;
+                p.Comments = "This is my name for " + System.DateTime.Now.Ticks.ToString();
+                Persons.Add(p);
+            }
+            NotifyPropertyChanged("MyCollection");
+        }
+
+        public UserList()
+        {
+            _myTimer = new Timer(100);
+            _myTimer.Elapsed += _myTimer_Elapsed;
+            _myTimer.Enabled = true;
+        }
+
+        void _myTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            genData();
+        }
+    }
+}
